c22d6f4d07eea66a3cb92949e3bfeffdaeb81009
Crysteier <crysteier12345@gmail.com>
2021-02-18 00:32:45 +0100

    cviko1 hotove

--- Cviko1/src/cviko1/Cviko1.java
+++ Cviko1/src/cviko1/Cviko1.java
@@ -16,7 +16,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
-import static org.eclipse.persistence.jpa.rs.util.JPARSLogger.exception;
+
 
 /**
  *
